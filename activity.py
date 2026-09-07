7c2304307949a5e669d7c1ac5a46acce49f82007
Desola-create <ekinomoohikhueme@gmail.com>
2026-09-07 14:24:54 +0100

    countries

---
+++ activity.py
@@ -0,0 +1,5 @@
+countries = {
+    "Nigeria": {"capital": "Abuja", "population": 200000000},
+    "Ghana": {"capital": "Accra", "population": 30000000},
+    "Kenya": {"capital": "Nairobi", "population": 50000000}
+}
